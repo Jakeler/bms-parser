0b2682e2a37c3ba4e6b2e5e36e6b4c2fbec3f4cf
Jake <git@ja-ke.tech>
2020-03-27 20:59:08 +0100

    Test dump in json

--- py/test.py
+++ py/test.py
@@ -15,7 +15,7 @@ from enum import Enum
 #     return out
 
 def isBasicType(obj: object):
-    return type(obj) in (int, float, bool, bytes, str)
+    return type(obj) in (int, float, bool, str)
 
 def serialize(obj: object):
     """ Recursive from object to dict"""
@@ -33,6 +33,8 @@ def serialize(obj: object):
         # Dump basic types
         if isBasicType(val):
             out[key] = val
+        if type(val) == bytes:
+            out[key] = val.hex()
         elif type(val) == list:
             out[key] = [serialize(i) for i in val]
         else:
@@ -44,31 +46,31 @@ def serialize(obj: object):
 d = Packet.from_bytes(b"\xdd\x03\x00\x1b\x118\x00b\x00\xa4\x04\xb0\x00\x00'n\x02\x82\x00\x00" + b'\x00\x00!\x0e\x03\x0b\x02\x0b"\x0b\x10\xfcBw')
 d = Packet.from_bytes(bytes.fromhex('dd 03 00 1b 10 3c fe 0e 02 d7 04 b0 00 00 27 6e 00 00 00 00 00 00 21 3d 03 0b 02 0b 37 0b 47 fb 69 77'))
 
-print(serialize(d))
+pp(json.dumps(serialize(d)))
 
 
 # Cells
 d2 = Packet.from_bytes(b'\xdd\x04\x00\x16\x0f\xa7\x0f\xa5\x0f\xa1\x0f\x98\x0f\x9e\x0f\xa0\x0f\xb1\x0f\xbb' + b'\x0f\xb1\x0f\xa6\x0f\xa7\xf8\x18w',)
-# pp(recurse_vars(d2))
+pp(json.dumps(serialize(d2)))
 cells = [c.volt for c in d2.body.data.cells]
-print(cells, sum(cells))
+print(sum(cells))
 
 # HW
 d2 = Packet.from_bytes(b'\xdd\x05\x00\x11SP15S001-P13S-30' + b'A\xfb\xfdw')
-# pp(recurse_vars(d2))
+pp(json.dumps(serialize(d2)))
 
 # Settings
 d2 = Packet.from_bytes(b'\xdd\x10\x00\x02\x07\xd0\xff\x27\x77')
-# pp(recurse_vars(d2))
+pp(json.dumps(serialize(d2)))
 # pp(vars(d2.body))
 
 # Request
 d2 = Packet.from_bytes(b'\xdd\xa5\x05\x00\xff\xfbw')
-# pp(recurse_vars(d2))
+pp(json.dumps(serialize(d2)))
 pp(isinstance(d2.body, Packet.ReadReq))
 
 # Write
 d2 = Packet.from_bytes(b'\xDD\x5A\x10\x02\x4E\x20\xFF\x80\x77')
-# pp(recurse_vars(d2))
+pp(json.dumps(serialize(d2)))
 pp(isinstance(d2.body, Packet.WriteReq))
 
