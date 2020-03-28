c123fb6d1a632ba3c0cb41253df93a118988fbf7
Jake <git@ja-ke.tech>
2020-03-28 02:29:14 +0100

    Move to str function into converter

--- py/converter.py
+++ py/converter.py
@@ -51,3 +51,28 @@ def serialize(obj: object) -> dict:
         else:
             out[key] = serialize(val)
     return out
+
+def pktToString(pkt: packet.Packet):
+    res = f'Packet with {pkt.cmd}'
+
+    if isinstance(pkt.body, packet.Packet.ReadReq):
+        res = f'Read request, ID {pkt.body.req_cmd}'
+    elif isinstance(pkt.body, packet.Packet.WriteReq):
+        res = f'Write request, ID {pkt.body.req_cmd}'
+    elif isinstance(pkt.body, packet.Packet.Response):
+        data = pkt.body.data
+        res = f'Response {pkt.body.status.name}, Type {data.__class__.__name__}: '
+        if isinstance(data, packet.BasicInfo):
+            res += f'{data.total.volt} V, {data.current.amp} A, '
+            res += f'{data.cell_count} Cells, {data.cycles} Cycles, T {[(str(t.celsius)+" °C") for t in data.temps]}\n'
+            res += f"BAL {['B' if c else '-' for c in data.balance_status.flag[:]]} \n"
+            res += f'{data.remain_cap_percent} %, {data.remain_cap.amp_hour} / {data.typ_cap.amp_hour} Ah\t'
+            res += f'FET: CHG={data.fet_status.charge.name} DIS={data.fet_status.discharge.name}'
+        elif isinstance(data, packet.CellVoltages):
+            res += ', '.join([(str(c.volt)+" V") for c in data.cells])
+        elif isinstance(data, packet.Hardware):
+            res += data.version
+        elif isinstance(data, bytes):
+            res += f'{data} ({int.from_bytes(data, byteorder="big")} = 0x{data.hex()})'
+    
+    return res
--- py/run.py
+++ py/run.py
@@ -1,9 +1,12 @@
 import os, select, time, argparse
 from kai import packet
 from kai.packet import Packet
+from converter import DB, pktToString
 
 parser = argparse.ArgumentParser(description='Basic BMS readout')
 parser.add_argument('dev', help='Serial Port')
+parser.add_argument('-l', '--log', dest='logfile', help='Log to file')
+parser.add_argument('-m', '--mongo', dest='mongo', help='Log to mongodb instance')
 args = parser.parse_args()
 
 intervall = 0.5
@@ -13,33 +16,10 @@ class Requests:
     cells = b'\xdd\xa5\x04\x00\xff\xfcw'
     hw = b'\xdd\xa5\x05\x00\xff\xfbw'
 
-def parse(data) -> str:
+def parse(data: bytes) -> str:
     try:
         pkt = Packet.from_bytes(data)
-        res = f'Packet with {pkt.cmd}'
-
-        if isinstance(pkt.body, Packet.ReadReq):
-            res = f'Read request, ID {pkt.body.req_cmd}'
-        elif isinstance(pkt.body, Packet.WriteReq):
-            res = f'Write request, ID {pkt.body.req_cmd}'
-        elif isinstance(pkt.body, Packet.Response):
-            data = pkt.body.data
-            res = f'Response {pkt.body.status.name}, Type {data.__class__.__name__}: '
-            if isinstance(data, packet.BasicInfo):
-                res += f'{data.total.volt} V, {data.current.amp} A, '
-                res += f'{data.cell_count} Cells, {data.cycles} Cycles, T {[(str(t.celsius)+" °C") for t in data.temps]}\n'
-                res += f"BAL {['B' if c else '-' for c in data.balance_status.flag[:11]]} \n"
-                res += f'{data.remain_cap_percent} %, {data.remain_cap.amp_hour} / {data.typ_cap.amp_hour} Ah\t'
-                res += f'FET: CHG={data.fet_status.charge.name} DIS={data.fet_status.discharge.name}'
-            elif isinstance(data, packet.CellVoltages):
-                res += ', '.join([(str(c.volt)+" V") for c in data.cells])
-            elif isinstance(data, packet.Hardware):
-                res += data.version
-            elif isinstance(data, bytes):
-                res += f'{data} ({int.from_bytes(data, byteorder="big")} = 0x{data.hex()})'
-        
-        
-        return res
+        pktToString(pkt)
     except Exception as e:
         return f'Failed: {e}'
 
